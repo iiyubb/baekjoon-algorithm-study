6bfedbb3e51af24c607e970a6b82aadd84c0a24c
iiyubb <hiyubin.lee@gmail.com>
2025-06-17 21:55:27 +0900

    완주하지못한선수 / 레벨1 / 23분 / O

---
+++ week22/완주하지못한선수_이유빈/Solution.java
@@ -0,0 +1,21 @@
+package 완주하지못한선수_이유빈;
+
+import java.util.Arrays;
+
+class Solution {
+  public String solution(String[] participant, String[] completion) {
+    String answer = "";
+
+    Arrays.sort(participant);
+    Arrays.sort(completion);
+
+    for (int i=0; i<completion.length; i++) {
+      if (!participant[i].equals(completion[i])) {
+        answer = participant[i];
+        return answer;
+      }
+    }
+    answer = participant[participant.length-1];
+    return answer;
+  }
+}
